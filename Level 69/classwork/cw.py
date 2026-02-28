7d1751cdb91a055370abbe6b07f15befb75e314a
tsagareishviliano38@gmail.com <tsagareishviliano38@gmail.com>
2026-02-28 14:48:24 +0400

    commit

---
+++ Level 69/classwork/cw.py
@@ -0,0 +1,17 @@
+# 1
+# def number_to_string(num):
+#     return str(num)
+
+# 2
+# def make_negative( number ):
+#     if number > 0:
+#         return -number
+#     else:
+#         return number
+
+# 3
+# def bool_to_word(boolean):
+#     if boolean:
+#         return "Yes"
+#     else:
+#         return "No"
