6a2a8349a36adfc599cf52d04366380ca1190866
tsagareishviliano38@gmail.com <tsagareishviliano38@gmail.com>
2026-03-21 20:33:56 +0400

    commit

---
+++ Level 070/classwork/cw.py
@@ -0,0 +1,43 @@
+# 1
+def greet():
+    return "hello world!"
+# 2
+def summation(num):
+    total = 0          
+    for i in range(1, num + 1): 
+        total += i      
+    return total         
+# 3
+def sum_array(a):
+    return sum(a)
+
+# 4
+def digitize(n):
+    number_str = str(n)
+    
+    result = []
+    for digit in number_str[::-1]:
+        result.append(int(digit))
+    
+    return result
+
+# 5
+def are_you_playing_banjo(name):
+    if name[0].lower() == 'r':
+        return name + " plays banjo"
+    else:
+        return name + " does not play banjo"
+# 6
+def abbrev_name(name):
+    words = name.split()
+    first = words[0][0].upper()
+    second = words[1][0].upper()
+    return first + "." + second
+# 7
+def find_average(numbers):
+    if not numbers:
+        return 0
+    return sum(numbers) / len(numbers)
+# 8
+def smash(words):
+    return ' '.join(words)
